26a38a2e81aa5836a640cd77af9bf5221602c934
Michael Wortman <imcfly@mac.com>
2018-01-03 22:16:28 -0800

    removed print statement

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     
     @IBAction func changeTitleButtonTapped(_ sender: Any) {
         tapCount += 1
-        print(tapCount)
         
         if tapCount >= 10 {
             
