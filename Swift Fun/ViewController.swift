08b213a6d882bf1677f68445a8256b5e0b0db594
Michael Wortman <imcfly@mac.com>
2018-01-03 22:14:35 -0800

    First commit as gitters!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tapCount = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var changeTitleButton: UIButton!
+    
+    @IBAction func changeTitleButtonTapped(_ sender: Any) {
+        tapCount += 1
+        print(tapCount)
+        
+        if tapCount >= 10 {
+            
+            view.backgroundColor = UIColor.gray
+            
+            titleLabel.text = "Differential Consulting"
+            titleLabel.textColor = UIColor.white
+            
+            tapCount = 0
+        }
+        
+        changeTitleButton.setTitle("Tap me \(10 - tapCount) more times!", for: [])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
